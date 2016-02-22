b071700a6a2477835df43d0330ce615031d66b74
Alex Votry <alexvotry@gmail.com>
2016-02-21 18:23:13 -0800

    complete flashing assignment

---
+++ flashing.js
@@ -0,0 +1,54 @@
+
+
+(function () {
+function randomColors() {
+  var hex = '123456789ABCDEF';
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 15)];
+  }
+  return color;
+}
+var createDiv = function() {
+  var squares = document.createElement('div');
+  squares.className = "rand";
+  squares.style.backgroundColor = randomColors();
+  squares.style.boxSizing = 'border-box';
+  squares.style.paddingBottom = '11.1%';
+  squares.style.width = '11.1%';
+  squares.style.float = 'left';
+  return squares;
+}
+
+function addSquare() {
+  var body = document.getElementsByTagName('body')[0];
+  var newSquares = [];
+  for (var i = 0; i < 63; i++) {
+    newSquares.push(createDiv());
+    body.appendChild(newSquares[i]);
+  }
+  return newSquares;
+}
+
+ addSquare();
+
+ console.log(createDiv().style.backgroundColor);
+
+ function flashSquare() {
+   var body = document.getElementsByTagName('body')[0];
+   var newSquares = document.getElementsByClassName('rand');
+   console.log(newSquares[2]);
+   for (var i = 0; i < 63; i++) {
+   newSquares[i].style.backgroundColor = randomColors();
+   }
+   return newSquares;
+ }
+
+
+
+ setInterval(function() {
+     flashSquare();
+ }, 2000 ); //set an interval timer up to repeat the function
+
+
+}());
--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-// Each tile should be a `div`
-// * Each tile's width is `11.1%`
-// * Set each tile's `float` property to `left`
-// * Each tile's paddingBottom is `11.1%`
-
-
 
 (function () {
-  // var body = document.getElementsByTagName('body')[0];
-  // for (var i = 0; i < 9; i++) {
-  //   var checkerBoard = [];
-  //   var rows = document.createElement('div');
-  //   rows.className = "rows";
-  //   checkerBoard.push(rows[i]);
-  // }
 var createDiv = function() {
   var squares = document.createElement('div');
   squares.className = "red";
@@ -23,10 +10,6 @@ var createDiv = function() {
   squares.style.float = 'left';
   return squares;
 }
-// var blackSq() {
-//   var squares = createDiv(
-//
-// }
 
 function addSquare() {
   var body = document.getElementsByTagName('body')[0];
@@ -41,6 +24,7 @@ function addSquare() {
 function makeBlack() {
   var squares = document.getElementsByTagName('div');
   for (var i = 0; i < squares.length; i++) {
+// debugging shows 'cannot set property 'className' of undefined.'
     squares[i*2].className = "black";
     squares[i*2].style.backgroundColor = "black";
   }
@@ -49,5 +33,3 @@ function makeBlack() {
 makeBlack();
 
 }());
-
-console.log(checkerBoard);
